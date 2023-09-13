69b776d71e07381338ac9c0a5a9f704992b9644d
Fernando Jimenez <jimenezfernando98@hotmail.com>
2023-09-13 00:12:37 -0600

    Se agregan las macros para poder representar los tiempos y ciclos del temporizador, ademas ademas se agregan cvariables globales, nuevas funciones y se configura el temporizador

--- src/semaforo.c
+++ src/semaforo.c
@@ -1,22 +1,41 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+
+/* MACROS */
+// Definiciones para tiempos y ciclos de temporizador
+#define TIMER_CYCLES_RESET 0
+#define ONE_SECOND 1
+#define THREE_SECONDS 3
+#define TEN_SECONDS 10
+#define HALF_SECOND_CYCLE 30
+#define FULL_SECOND_CYCLE 60
+#define FULL_SECOND_CYCLE_RESET 63
+/* ESTADOS */
+// Enumeración para representar los estados del semáforo
 typedef enum {
     PASS_VEHICLE,
+    BLINK_VEHICLE,
     STOP_VEHICLE,
     PASS_PEDESTRIAN,
+    BLINK_PEDESTRIAN,
     STOP_PEDESTRIAN
 } State;
 
+// Variable de estado actual
 State estado;
 
 /* VARIABLES GLOBALES */
 int pressed = 0;
-
+int timerCycles = 0;
+int seconds = 0;
 /* DECLARACIÓN DE FUNCIONES */
 // Prototipos de funciones
 void setup();
 void FSM();
+inline void interruptionRoutine();  
+inline void blinkingRoutine();  
+
 /* INTERRUPCIONES */
 // Interrupción para el botón
 ISR(INT0_vect){
@@ -89,5 +108,47 @@ void FSM(){
     default:
       estado = PASS_VEHICLE;
       break;
+  }
+}
+// Función para manejar el parpadeo de las luces del semáforo en los estados BLINK_VEHICLE y BLINK_PEDESTRIAN
+inline void blinkingRoutine(){
+  switch (estado){
+    // Cuando el semáforo para vehículos debe parpadear
+    case BLINK_VEHICLE:
+      // Parpadea la luz del semáforo para vehículos (PB0) cada medio segundo o segundo completo
+      if(timerCycles == HALF_SECOND_CYCLE || timerCycles == FULL_SECOND_CYCLE){
+        PORTB ^= (1<<PB0);
+      }
+      break;
+
+    // Cuando el semáforo para peatones debe parpadear
+    case BLINK_PEDESTRIAN:
+      // Parpadea la luz del semáforo para peatones (PB3) cada medio segundo o segundo completo
+      if(timerCycles == HALF_SECOND_CYCLE || timerCycles == FULL_SECOND_CYCLE){
+        PORTB ^= (1<<PB3);
+      }
+      break;
+
+    // Caso por defecto
+    default:
+      // No hacer nada si el estado no es ninguno de los anteriores
+      break;
+  }
+}
+
+// Función que se ejecuta en cada interrupción del Timer
+inline void interruptionRoutine(){
+  // Llamar a la función para manejar el parpadeo de las luces
+  blinkingRoutine();
+  
+  // Si se ha alcanzado un ciclo completo de un segundo
+  if(timerCycles == FULL_SECOND_CYCLE_RESET){
+    // Incrementa el contador de segundos
+    seconds++;
+    // Reiniciar el contador de ciclos del temporizador
+    timerCycles = TIMER_CYCLES_RESET;
+  } else {
+    // Incrementar el contador de ciclos del temporizador
+    timerCycles++;
   }
 }
